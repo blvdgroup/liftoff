a5b77bed17ff2c699cd12052d4d9285ced193f6b
Kyle <kyle@vulpine.me>
2018-06-06 14:41:41 +0200

    build: swap from tsc directly to using babel

---
+++ .babelrc.js
@@ -0,0 +1,32 @@
+const NODE_VERSION = "10.3" // TODO - depend on NODE_VERSION file?
+
+const buildEnv = process.env.LIFTOFF_BUILDTARG
+const envTargets = {
+  client: {
+    "browsers": ["> 5%", "last 2 versions"] // TODO - will eventually widen the
+                                            // range and see how it goes
+  },
+  server: {
+    "node": NODE_VERSION
+  }
+}
+
+const dev = NODE_ENV === 'deveoplment'
+
+const presets = [
+  "@babel/preset-typescript",
+  [
+    "@babel/preset-react",
+    {
+      development: dev
+    }
+  ]  
+  [
+    "@babel/preset-env",
+    {
+      "targets": envTargets[buildEnv]
+    }
+  ]
+]
+
+module.exports = { presets }
