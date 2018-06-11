dd1262568b23f1c2c8fc2a3e2307b74e95953515
Kyle <kyle@vulpine.me>
2018-06-11 20:39:58 +0200

    revert(ssr): universal-component is not worth it
    This reverts commit 84fa825dd8cef6064c47639614fd6ed1ae98810c.

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,11 @@
 import Koa from 'koa'
-import { sep } from 'path'
 
 import ssr from './ssr'
 
 const app = new Koa()
 
 app.use(ctx => {
-  let path = ctx.request.path.substring(1).replace('/', sep)
-  if (path === '') path = 'index'
-  ctx.body = ssr(path)
+  ctx.body = ssr('index')
   ctx.type = 'text/html'
 })
 
